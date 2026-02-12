6fc1581e2ed4306e6d10eb3616d8ae6c0785ef8a
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-12 14:03:01 -0600

    Fix: Corrige las rutas hardcoded en los enlaces del menú

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <br/>
-<img src="https://semillasdelagro.com/pruebas/sistema_obsequios/logo_mandara.svg" width="150px" style="display: block; margin: auto;">
+<img src="https://semillasdelagro.com/pruebas/sistema_mandara/logo_mandara.svg" width="150px" style="display: block; margin: auto;">
 <br/>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid" id="menu">
@@ -7,19 +7,19 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="https://semillasdelagro.com/pruebas/sistema_obsequios/">Mandara</a>
+      <a class="navbar-brand" href="https://semillasdelagro.com/pruebas/sistema_mandara/">Mandara</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/sistema_obsequios/">Inicio</a>
+          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/sistema_mandara/">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/admin_mandara/">Clientes</a>
+          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/sistema_mandara/">Clientes</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/sistema_obsequios/regalo.php">Regalo</a>
+          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/sistema_mandara/regalo.php">Regalo</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/admin_mandara/lista_citas.php">Citas</a>
+          <a class="nav-link" aria-current="page" href="https://semillasdelagro.com/pruebas/sistema_mandara/lista_citas.php">Citas</a>
         </li>
        
         <li class="nav-item dropdown">
@@ -27,10 +27,10 @@
             Tienda
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="https://semillasdelagro.com/pruebas/admin_mandara/tienda/lista_categoria.php">Categorias</a></li>
-            <li><a class="dropdown-item" href="https://semillasdelagro.com/pruebas/admin_mandara/tienda/lista_productos.php">Productos</a></li>
+            <li><a class="dropdown-item" href="https://semillasdelagro.com/pruebas/sistema_mandara/tienda/lista_categoria.php">Categorias</a></li>
+            <li><a class="dropdown-item" href="https://semillasdelagro.com/pruebas/sistema_mandara/tienda/lista_productos.php">Productos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="https://semillasdelagro.com/pruebas/admin_mandara/tienda/ver_productos.php">Ver tienda</a></li>
+            <li><a class="dropdown-item" href="https://semillasdelagro.com/pruebas/sistema_mandara/tienda/ver_productos.php">Ver tienda</a></li>
           </ul>
         </li>
        
