d024e28f6e317eecdd9ec7ef4d8b9ab554e1ff20
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2026-02-12 18:52:54 -0600

    Debug: Habilita el reporte de errores y añade cabeceras anti-caché a regalos/regalo.php

--- admin_mandara/regalos/regalo.php
+++ admin_mandara/regalos/regalo.php
@@ -1,3 +1,11 @@
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
